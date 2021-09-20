40914a621b07bd71e9465fe8368ea5286ec300df
kelsonfox <kelsonbatista@yahoo.co.uk>
2021-09-20 02:16:51 -0300

    Parte 9

---
+++ js/main.js
@@ -0,0 +1,19 @@
+function showWatchnow() {
+    var element = document.getElementById("watchnow");
+    element.classList.add("watchnow-show");
+}
+
+function hideWatchnow() {
+    var element = document.getElementById("watchnow");
+    element.classList.remove("watchnow-show");
+}
+
+function showMoreinfo() {
+    var element = document.getElementById("moreinfo");
+    element.classList.add("moreinfo-show");
+}
+
+function hideMoreinfo() {
+    var element = document.getElementById("moreinfo");
+    element.classList.remove("moreinfo-show");
+}
